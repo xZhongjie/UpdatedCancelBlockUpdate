4e826c3d72fc36e620d6a9f42654157664634f1d
xZhongjie <xzhongjie@users.noreply.github.com>
2026-08-19 17:07:27 +0800

    MPL-2.0 合规: 源码文件头添加许可证声明与修改标注 (保留原作者 Phoupraw)

--- src/main/java/phoupraw/mcmod/cancelblockupdate/mixin/MAbstractRailBlock.java
+++ src/main/java/phoupraw/mcmod/cancelblockupdate/mixin/MAbstractRailBlock.java
@@ -1,3 +1,12 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * Modified from CancelBlockUpdate (https://github.com/Phoupraw/CancelBlockUpdate)
+ * by xZhongjie (https://github.com/xZhongjie/UpdatedCancelBlockUpdate)
+ */
+
 package phoupraw.mcmod.cancelblockupdate.mixin;
 
 import net.minecraft.block.AbstractRailBlock;
