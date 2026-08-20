f4876ac2239bbaa9f6989f48eba8ab713e4bb27b
xZhongjie <64800332+xZhongjie@users.noreply.github.com>
2026-08-21 01:14:16 +0800

    更新模组信息：修改版说明、新增指令与贡献者，修复中文编码

--- src/main/java/phoupraw/mcmod/cancelblockupdate/datagen/English.java
+++ src/main/java/phoupraw/mcmod/cancelblockupdate/datagen/English.java
@@ -11,7 +11,7 @@ package phoupraw.mcmod.cancelblockupdate.datagen;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import phoupraw.mcmod.cancelblockupdate.CancelBlockUpdate;
 import phoupraw.mcmod.cancelblockupdate.registry.CBUGameRules;
@@ -19,8 +19,8 @@ import phoupraw.mcmod.cancelblockupdate.registry.CBUGameRules;
 @Environment(EnvType.CLIENT)
 final class English extends FabricLanguageProvider {
 
-    English(FabricDataOutput dataOutput) {
-        super(dataOutput);
+    English(FabricDataGenerator dataGenerator) {
+        super(dataGenerator);
     }
 
     @Override
@@ -29,14 +29,16 @@ final class English extends FabricLanguageProvider {
         b.add("modmenu.nameTranslation." + CancelBlockUpdate.MOD_ID, modName);
         b.add("modmenu.descriptionTranslation." + CancelBlockUpdate.MOD_ID, """
           §4§l[Warning]§r During world generation enable this mod might produce lots of floating blocks, if want to generate normal world, please change game rule in advance.
+          This is an updated maintenance fork of CancelBlockUpdate by Phoupraw, maintained by xZhongjie, with support for newer Minecraft versions.
           Cancel block update, block schedule tick, random tick and fluid schedule tick, allow unlimited place block.
           For building special buildings, make and test special map.
           §lNew game rules:§r
           - §ocancelblockupdate:off§r: when §ofalse§r, cancel all block update; when §otrue§r it is vanilla; default §ofalse§r.
           - §ocancelblockupdate:replace§r: when §ofalse§r, grass, fern and so on block can't be replaced directly; when §otrue§r it is vanilla; default §ofalse§r.
+          - §ocancelblockupdate:stack_slabs§r: when §otrue§r, slabs can be stacked into double slabs; default §otrue§r.
           §lNew commands:§r
           - §o/cancelblockupdate random <pos>§r: trigger random tick of block at §opos§r.
-          - §o/cancelblockupdate schedule <pos>§r：trigger schedule tick of block at §opos§r.
+          - §o/cancelblockupdate schedule <pos>§r: trigger schedule tick of block at §opos§r.
           Suggest with adapt block state debug stick to use.
           """);
         b.add(CBUGameRules.OFF.getTranslationKey(), modName + ": disable all effects of the mod");
