9f65f23f5204b8e85dee55b305f94081b7e9db0c
xZhongjie <64800332+xZhongjie@users.noreply.github.com>
2026-09-07 22:29:37 +0800

    修复游戏规则缓存空指针崩溃

--- src/main/java/phoupraw/mcmod/cancelblockupdate/registry/CBUGameRules.java
+++ src/main/java/phoupraw/mcmod/cancelblockupdate/registry/CBUGameRules.java
@@ -119,7 +119,7 @@ public final class CBUGameRules {
      @return ??????
      */
     public static boolean get(GameRule<Boolean> key, WorldView world) {
-        var cache = CACHES.get(key);
+        var cache = CACHES.computeIfAbsent(key, ignored -> new WeakHashMap<>());
         Boolean value = cache.get(world);
         if (value != null) return value;
         if (world instanceof ServerWorldAccess serverWorldAccess) {
